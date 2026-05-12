95d46be6824e95ea680ea10778fca89ceb9691cc
javierlanuevacepa <134305861+javierlanuevacepa@users.noreply.github.com>
2026-05-11 22:15:56 -0400

    Add files via upload

--- src/main/java/com/_dmicroservicios/demo/Controller/AuthController.java
+++ src/main/java/com/_dmicroservicios/demo/Controller/AuthController.java
@@ -5,21 +5,58 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.security.authentication.AuthenticationManager;
 
+import com._dmicroservicios.demo.Dto.LoginRequest;
+import com._dmicroservicios.demo.Security.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 
 @RestController
-@RequestMapping("/v0/auth")
+@RequestMapping("/auth")
 public class AuthController {
-    
 
-    private final AuthenticationManager authenticationManager;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-  
+    @Autowired
+    private JwtUtil jwtUtils;
 
-    public AuthController(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-      
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        
+        try {
+            // 1. Spring Security intentará autenticar usando el usuario en memoria
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+            );
+
+            // 2. Si las credenciales son correctas, obtenemos los detalles del usuario
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+            // 3. Generamos el token usando tu clase JwtUtils
+            String token = jwtUtils.generateToken(userDetails);
+
+            // 4. Retornamos el token como un JSON
+            return ResponseEntity.ok(Map.of(
+                    "mensaje", "Autenticación exitosa",
+                    "token", token,
+                    "username", userDetails.getUsername(),
+                    "password", userDetails.getPassword()
+            ));
+
+        } catch (Exception e) {
+            // Si la contraseña o el usuario son incorrectos, caerá aquí
+            return ResponseEntity.status(401).body(Map.of(
+                    "error", "Credenciales incorrectas"
+            ));
+        }
     }
 
-
+    
 }
