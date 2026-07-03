a6463017c1e9300063271ed1358b0cec172beaf9
javierlanuevacepa <134305861+javierlanuevacepa@users.noreply.github.com>
2026-07-03 01:14:46 -0400

    Add files via upload

---
+++ SqsService.java
@@ -0,0 +1,44 @@
+package com._dmicroservicios.demo.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com._dmicroservicios.demo.Model.AuditoriaEvent;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+@Service
+public class SqsService {
+
+    @Value("${aws.sqs.queue-url}")
+    private String queueUrl;
+
+    private final SqsClient sqsClient;
+    private final ObjectMapper objectMapper;
+
+    public SqsService(SqsClient sqsClient, ObjectMapper objectMapper) {
+        this.sqsClient = sqsClient;
+        this.objectMapper = objectMapper;
+    }
+
+    public void enviarAuditoria(AuditoriaEvent evento) {
+
+        try {
+
+            String json = objectMapper.writeValueAsString(evento);
+
+            SendMessageRequest request = SendMessageRequest.builder()
+                    .queueUrl(queueUrl)
+                    .messageBody(json)
+                    .build();
+
+            sqsClient.sendMessage(request);
+
+            System.out.println("Mensaje enviado a SQS.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
